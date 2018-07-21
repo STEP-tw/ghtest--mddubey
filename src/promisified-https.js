1f278bd8758f288c6167d451ee729d0dc43805da
MD Dubey <mddubey409@gmail.com>
2018-07-21 17:41:31 +0530

    Promisify node https.

---
+++ src/promisified-https.js
@@ -0,0 +1,20 @@
+const https = require('https');
+
+exports.promisifiedHttps = {
+	post: (options, body) => {
+		return new Promise((resolve, reject) => {
+			const request = https.request(options, (response) => {
+				response.setEncoding('utf-8');
+				if (response.statusCode < 200 || response.statusCode > 299) {
+					reject(new Error(`Unexpected response form server ${response.statusCode}`));
+				};
+				var responseContent = '';
+				response.on('data', (d) => {responseContent += d});
+				response.on('end', () => {resolve(responseContent)})
+			})
+			request.write(JSON.stringify(body))
+			request.on('error', (err) => {reject(err)})
+			request.end()
+		});
+	}
+}
