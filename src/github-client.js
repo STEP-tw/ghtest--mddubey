8ed48a398d129dd9e2f2a832af3ea8dcd4265125
MD Dubey <mddubey409@gmail.com>
2018-07-22 20:44:37 +0530

    Dubey | 000 | Enable report to give latest repos for given username.

--- src/github-client.js
+++ src/github-client.js
@@ -1,30 +1,59 @@
 const promisifiedHttps = require('./promisified-https.js').promisifiedHttps
+const graphqlQueries = require('./graphql-queries.js').graphqlQueries
+
+const requestOptions = {
+	'hostname': 'api.github.com',
+	'path': '/graphql',
+	'method': 'POST',
+	'headers': {
+		'Content-Type': 'application/json',
+		'Authorization': 'bearer ' + process.env.GITHUB_TOKEN,
+		'User-Agent': 'graphql client'
+	}
+};
 
 
 exports.githubClient = {
 	getUser: () => {
-		var options = {
-			'hostname': 'api.github.com',
-			'path': '/graphql',
-			'method': 'POST',
-			'headers': {
-				'Content-Type': 'application/json',
-				'Authorization': 'bearer ' + process.env.GITHUB_TOKEN,
-				'User-Agent': 'graphql client'
-			}
-		}
 		var data = {
 			'query': "query { viewer { login }}",
 			'variables': {}
 		};
 		return promisifiedHttps
-			.makeRequest(options, data)
+			.makeRequest(requestOptions, data)
 			.then(response => new Promise(resolve => {
 				responseJson = JSON.parse(response)
 				return resolve(responseJson.data.viewer.login)
 			}))
 			.catch(err => {
 				console.log("Error while fetching current user details");
+				throw err;
+			})
+	},
+
+	fetchUserRepos: (username) => {
+		var data = {
+			'query': graphqlQueries.getUserRepoQuery,
+			'variables': {username:username}
+		}
+
+		return promisifiedHttps
+			.makeRequest(requestOptions, data)
+			.then(response => new Promise((resolve, reject) => {
+				responseJson = JSON.parse(response).data;
+				if (!responseJson.user) {
+					reject(new Error(`No Such User ${username}`));
+				};
+				repositories = responseJson.user.repositories
+				//As of now last 30 repos. Might need to paginate
+				var reportData = {
+					total: repositories.totalCount,
+					repos: repositories.edges.map(edge => edge.node.name)
+				}
+				return resolve(reportData)
+			}))
+			.catch(err => {
+				console.log("Error while fetching repos for", username);
 				throw err;
 			})
 	}
--- src/report.js
+++ src/report.js
@@ -1,10 +1,23 @@
 const githubClient = require('./github-client.js').githubClient
 
-const generateGithubReportCard = function() {
+
+const generateGithubReportCard = function(username) {
+	//As of now these two are indepedent
 	githubClient
 		.getUser()
 		.then(username => console.log("Hello", username))
 		.catch(err => console.log(err))
-}
 
-generateGithubReportCard();
+	githubClient
+		.fetchUserRepos(username)
+		.then(reportData => {
+			console.log("****************************")
+			console.log("Information about",username);
+			console.log("Number of all repos",reportData.total)
+			console.log("Latest 30 repos", reportData.repos)
+			console.log("****************************")
+		})
+		.catch(err => console.log(err))
+}
+username = process.argv[2]
+generateGithubReportCard(username);
